1f9f4ee87cfedc619e5022c1e5c0d01423b07af1
u6270944 <u6270944@anu.edu.au>
2019-01-16 16:47:54 +0800

    Successfully following the script

--- FakeProcess.py
+++ FakeProcess.py
@@ -1,14 +1,15 @@
 import Client
 
-# # 超时
-# Client.send('对本期完成的凭证进行提交处理')
-# print(Client.receive())
-# print(Client.receive())
-
-# 执行查看
+# 超时
 Client.send('对本期完成的凭证进行提交处理')
 print(Client.receive())
 print(Client.receive())
-Client.send('好')
 print(Client.receive())
+
+# # 执行查看
+# Client.send('对本期完成的凭证进行提交处理')
+# print(Client.receive())
+# print(Client.receive())
+# Client.send('好')
+# print(Client.receive())
 
